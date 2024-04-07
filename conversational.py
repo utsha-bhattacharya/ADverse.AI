41f496908bace589a11f595d4ca3ff28ccd64dee
Utsha Bhattacharya <122881418+utsha-bhattacharya@users.noreply.github.com>
2024-04-07 17:28:34 +0530

    conversational.py

--- conversational.py
+++ conversational.py
@@ -6,6 +6,10 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
 from langchain.chains.question_answering import load_qa_chain
 
+from authtoken import GOOGLE_API_KEY,OPENAI_API_KEY
+os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
 # Function to extract text from PDF documents
 def get_pdf_text(pdf_docs):
     text = ""
