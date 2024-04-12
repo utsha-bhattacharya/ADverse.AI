03c92f074811c87763f51ade56cf3f3cfa67f0c8
Utsha Bhattacharya <122881418+utsha-bhattacharya@users.noreply.github.com>
2024-04-12 15:13:45 +0530

    email.py

--- email.py
+++ email.py
@@ -1,5 +1,7 @@
 from conversational import get_email_chain
 import streamlit as st
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
 def generate_automated_email(recipient_name, recipient_email, email_context):
 
     # Generate email template using Gemini model
