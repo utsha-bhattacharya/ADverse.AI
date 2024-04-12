6c4f8106c8203ffcabfa590c9febc46dc3b703a4
Utsha Bhattacharya <122881418+utsha-bhattacharya@users.noreply.github.com>
2024-04-12 15:08:29 +0530

    email.py

---
+++ email.py
@@ -0,0 +1,13 @@
+from conversational import get_email_chain
+import streamlit as st
+def generate_automated_email(recipient_name, recipient_email, email_context):
+
+    # Generate email template using Gemini model
+    chain =  get_email_chain("gemini-pro")
+    response = chain(
+        {"recipient_name": recipient_name, "recipient_email": recipient_email, "email_context": email_context},
+        return_only_outputs=True
+    )
+    # Display the generated email template
+    st.subheader("Generated Email Template:")
+    st.write("Reply: ", response["text"])
