9bb609c361a2a888b9d91d7cdb6b0c8bb92e7b0a
Utsha Bhattacharya <122881418+utsha-bhattacharya@users.noreply.github.com>
2024-04-12 15:06:58 +0530

    conversational.py

--- conversational.py
+++ conversational.py
@@ -5,6 +5,7 @@ from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
 from langchain.chains.question_answering import load_qa_chain
+from langchain.chains import LLMChain
 
 # Function to extract text from PDF documents
 def get_pdf_text(pdf_docs):
