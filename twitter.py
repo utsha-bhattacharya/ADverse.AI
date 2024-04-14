438b52774f6f442b5e927fb76713395bfc4994c4
Utsha Bhattacharya <122881418+utsha-bhattacharya@users.noreply.github.com>
2024-04-15 00:19:37 +0530

    twitter.py

--- twitter.py
+++ twitter.py
@@ -4,7 +4,26 @@ from langchain.vectorstores import FAISS
 from conversational import get_conversational_chain
 
 # Function to generate Twitter post
-def generate_twitter_post(user_question):
+def generate_twitter_post(user_question, tone):
+    context = "Write a Twitter post highlighting important points in 1-2 Lines"
+    # Adjust context based on tone
+    if tone == 'Negative':
+        context = "Write a Twitter post highlighting important points in 1-2 Lines - Addressing Concerns"
+    elif tone == 'Positive':
+        context = "Write a Twitter post highlighting important points in 1-2 Lines - Highlighting Strengths"
+    elif tone == 'Formal':
+        context = "Write a formal Twitter post highlighting key points in 1-2 Lines"
+    elif tone == 'Academic':
+        context = "Compose an academic-style Twitter post, covering significant aspects in 1-2 Lines"
+    elif tone == 'Creative':
+        context = "Craft a creative Twitter post, exploring important points through imaginative language in 1-2 Lines"
+    elif tone == 'Simple':
+        context = "Draft a straightforward Twitter post, summarizing key aspects in 1-2 Lines"
+    elif tone == 'Critical Analysis':
+        context = "Provide a critical analysis in a Facebook post, examining important points in 1-2 Lines"
+    elif tone == 'Standard' or tone == 'Fluent':
+        context = "Write a Twitter post highlighting important points 1-2 Lines"
+
     embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
     new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
     docs = new_db.similarity_search(user_question)
