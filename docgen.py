b1f42953e607beefd4fe5b3c8687bbe895c4e635
Utsha Bhattacharya <122881418+utsha-bhattacharya@users.noreply.github.com>
2024-04-18 12:19:51 +0530

    docgen.py

--- docgen.py
+++ docgen.py
@@ -2,7 +2,7 @@ import streamlit as st
 from conversational import get_pdf_text, get_text_chunks,get_vector_store
 from linkedin import generate_linkedin_post
 from twitter import  generate_twitter_post
-from facebook import generate_facebook_post
+from facebook_gen import generate_facebook_post
 
 
 def generate_document_content(user_question, platform, pdf_docs, tone):
